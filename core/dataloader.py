5c604624e72643ba75cd1276e40411adb8840113
Arthur Jinyue Guo <arthur.jinyue.guo@gmail.com>
2024-07-17 10:19:42 +0200

    Add full mode

--- core/dataloader.py
+++ core/dataloader.py
@@ -17,9 +17,10 @@ class SoundActionsDataset(torch.utils.data.Dataset):
         root: str = "/fp/homes01/u01/ec-jinyueg/felles_/Research/Project/AMBIENT/Datasets/SoundActions/",
         video_path: str = "video-HD",
         audio_path: str = "wav",
-        label_path="labels/SoundActions_labeling_A.csv",
-        video_transform=None,
-        audio_transform=None,
+        label_path: str ="labels/SoundActions_labeling_A.csv",
+        video_transform = None,
+        audio_transform = None,
+        size: int = None,
     ):
         assert load_mode in ["preload", "online"]
         assert sample_mode in ["full"]
@@ -37,6 +38,28 @@ class SoundActionsDataset(torch.utils.data.Dataset):
         self.load_mode = load_mode
         self.sample_mode = sample_mode
 
+        if size is not None:
+            self.video_paths = self.video_paths[:size]
+            self.audio_paths = self.audio_paths[:size]
+            self.labels = self.labels.iloc[:size] 
+
+        if self.load_mode == "preload":
+            self.videos = []
+            self.videos_metadata = []
+            self.audios = []
+            self.audios_fps = []
+            print("=> Preloading SoundActions dataset...")
+            for video_path, audio_path in zip(self.video_paths, self.audio_paths):
+                video, _, video_metadata = torchvision.io.read_video(
+                    video_path, pts_unit="sec"
+                )
+                audio, audio_fps = torchaudio.load(audio_path)
+                self.videos.append(video)
+                self.videos_metadata.append(video_metadata)
+                self.audios.append(audio)
+                self.audios_fps.append(audio_fps)
+            print("=> Preloading done")
+
     def __len__(self):
         if self.sample_mode == "full":
             return len(self.video_paths)
@@ -51,6 +74,11 @@ class SoundActionsDataset(torch.utils.data.Dataset):
                 video_path, pts_unit="sec"
             )
             data["audio"], data["audio_fs"] = torchaudio.load(audio_path)
+        elif self.load_mode == "preload":
+            data["video"] = self.videos[index]
+            data["video_metadata"] = self.videos_metadata[index]
+            data["audio"] = self.audios[index]
+            data["audio_fs"] = self.audios_fps[index]
 
         if self.video_transform is not None:
             data["video"] = self.video_transform(data["video"])
@@ -60,7 +88,8 @@ class SoundActionsDataset(torch.utils.data.Dataset):
 
 
 if __name__ == "__main__":
-    dataset = SoundActionsDataset("train")
+    # dataset = SoundActionsDataset("train")
+    dataset = SoundActionsDataset("train", load_mode="preload", size=10)
     print(len(dataset))
     sample = dataset[0]
     print(sample["video"].shape)
