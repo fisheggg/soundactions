129a1ad62b0fa9b443002dbc1669ebfd0a1560ea
Jinyue Guo <jinyueg@ml1.hpc.uio.no>
2024-08-21 12:20:43 +0200

    Add video transforms

---
+++ soundactions/transform.py
@@ -0,0 +1,59 @@
+import torch
+from torch import nn
+from torchvision.transforms.v2 import functional as TF
+from torchvision.transforms.v2 import ColorJitter
+
+class VideoRandomHorizontalFlip(nn.Module):
+    """
+    Randomly flip the video horizontally with a given probability.
+    If flipped, all frames in the same video will be flipped, vice versa.
+    """
+    def __init__(self, p: float=0.5):
+        super().__init__() 
+        assert 0 <= p <= 1, f"p should be in [0, 1], got {p}"
+        self.p = p
+
+    def forward(self, video_batch):
+        """input shape: (B, T, C, H, W)"""
+        assert video_batch.dim() == 5, f"video shape: {video_batch.shape}"
+        for idx, video in enumerate(video_batch):
+            if torch.rand(1) < self.p:
+                video_batch[idx] = TF.horizontal_flip(video)
+            else:
+                pass
+
+        return video_batch
+
+
+class VideoColorJitter(nn.Module):
+    """
+    All frames in the same video will be applied with the same jittering parameters.
+    Same API as torchvision.transforms.ColorJitter.
+    """
+
+    def __init__(self, brightness, contrast, saturation, hue):
+        super().__init__()
+        self.b = brightness
+        self.c = contrast
+        self.s = saturation
+        self.h = hue
+
+    def forward(self, video_batch):
+        """input shape: (B, T, C, H, W)"""
+        assert video_batch.dim() == 5, f"video shape: {video_batch.shape}"
+        for idx, video in enumerate(video_batch):
+            fn_idx, brightness_factor, contrast_factor, saturation_factor, hue_factor = self.get_params(
+                self.b, self.c, self.s, self.h
+            )
+            for fn_id in fn_idx:
+                if fn_id == 0 and brightness_factor is not None:
+                    video_batch[idx] = TF.adjust_brightness(video, brightness_factor)
+                elif fn_id == 1 and contrast_factor is not None:
+                    video_batch[idx] = TF.adjust_contrast(video, contrast_factor)
+                elif fn_id == 2 and saturation_factor is not None:
+                    video_batch[idx] = TF.adjust_saturation(video, saturation_factor)
+                elif fn_id == 3 and hue_factor is not None:
+                    video_batch[idx] = TF.adjust_hue(video, hue_factor)
+
+        return video_batch
+
