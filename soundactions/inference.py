8f5edf153043b8460536fbc07a4c7f04ecd0673b
Arthur Jinyue Guo <arthur.jinyue.guo@gmail.com>
2024-08-31 13:10:44 +0200

    Fix embedding plots

--- soundactions/inference.py
+++ soundactions/inference.py
@@ -6,13 +6,18 @@ from tqdm import tqdm
 from pathlib import Path
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
+import scienceplots
+
+plt.style.use(["science", "no-latex"])
 
 sys.path.append(str(Path(__file__).resolve().parent))
 from dgsct import load_DGSCT
 from dataloader import SoundActionsDataset
 from finetune import LitDGSCT
+from inference_config import embeddings_config
 
 ## DG-SCT input shape:
+("PerceptionType",)
 # => audio shape: torch.Size([2, 10, 320000]), type: torch.float32
 # => video shape: torch.Size([2, 10, 3, 192, 192]), type: torch.float32
 # => gt shape: torch.Size([2, 10, 29]), type: torch.float32
@@ -52,7 +57,7 @@ def plot_embeddings(
     assert plot_modality in ["av", "a", "v"]
 
     print("=> Loading dataset...")
-    soundactions = SoundActionsDataset(load_mode="online", modality=train_modality)
+    soundactions = SoundActionsDataset(load_mode="online", modality=data_modality)
 
     print("=> Loading model...")
     if ckpt_path is None:
@@ -92,16 +97,24 @@ def plot_embeddings(
         colors.append(label_color_map[coloring_label][sample["label"][coloring_label]])
 
     if dr_alg == "pca":
-        out = PCA(n_components=2).fit_transform(embeds)
+        points = PCA(n_components=2).fit_transform(embeds)
     elif dr_alg == "tsne":
-        out = TSNE(n_components=2).fit_transform(embeds)
+        points = TSNE(n_components=2).fit_transform(embeds)
     elif dr_alg == "umap":
         pass
 
-    # plt.figure(figsize=(10, 10))
-    # plt.scatter(out[:, 0], out[:, 1], c=colors)
-    # plt.title(f"SoundActions embeddings from DG-SCT, {dr_alg}, {coloring_label}")
-    # plt.legend()
+    plt.figure(figsize=(5, 5), dpi=300)
+    for label in [0, 1, 2, 3]:
+        idx = np.where(np.array(labels) == label)
+        plt.scatter(
+            points[:, 0][idx], points[:, 1][idx], c=np.array(colors)[idx], label=label
+        )
+    plt.xticks([])
+    plt.yticks([])
+    if coloring_label == "PerceptionType":
+        plt.legend(["impulsive", "iterative", "sustain", "unknown"])
+    elif coloring_label == "Enjoyable":
+        plt.legend(["yes", "neutral", "no", "unknown"])
 
     if save_path is not None:
         plt.savefig(save_path)
@@ -111,11 +124,25 @@ def plot_embeddings(
 
 
 if __name__ == "__main__":
-    plot_embeddings(
-        train_modality="av",
-        data_modality="av",
-        plot_modality="av",
-        ckpt_path="/projects/ec12/jinyueg/SoundActions/soundactions/soundactions/logs/V04_all_PerceptionType_av_av/soundactions/0407syid/checkpoints/epoch=12-step=962.ckpt",
-        coloring_label="PerceptionType",
-        save_path="test_embeds.png",
-    )
+    # plot_embeddings(
+    #     train_modality="av",
+    #     data_modality="av",
+    #     plot_modality="av",
+    #     ckpt_path="/projects/ec12/jinyueg/SoundActions/soundactions/soundactions/soundactions/logs/V04_all_PerceptionType_av_av/soundactions/0407syid/checkpoints/epoch=12-step=962.ckpt",
+    #     coloring_label="PerceptionType",
+    #     save_path="test_embeds.png",
+    # )
+
+    # for config in tqdm(embeddings_config["cls_PerceptionType_av"]):
+    #     plot_embeddings(*config)
+    # for config in tqdm(embeddings_config["cls_PerceptionType_a"]):
+    #     plot_embeddings(*config)
+    # for config in tqdm(embeddings_config["cls_PerceptionType_v"]):
+    #     plot_embeddings(*config)
+
+    for config in tqdm(embeddings_config["cls_Enjoyable_av"]):
+        plot_embeddings(*config)
+    for config in tqdm(embeddings_config["cls_Enjoyable_a"]):
+        plot_embeddings(*config)
+    for config in tqdm(embeddings_config["cls_Enjoyable_v"]):
+        plot_embeddings(*config)
